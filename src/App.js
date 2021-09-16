e4013a5b3e0d3d04c116a977c701a52eed0fe350
hank <matheus_bueno10@hotmail.com>
2021-09-16 07:13:45 -0300

    Arrumado erro

--- src/App.js
+++ src/App.js
@@ -2,31 +2,27 @@ import react from "react";
 import "./App.css";
 import { useState, useEffect } from "react";
 import Characters from "./Components/Characters";
-import CharCard from "./Components/CharCard";
+
 function App() {
   const [characterList, setCharacterList] = useState([]);
-  const [next, setNext] = useState(0);
+  const [link, setLink] = useState(
+    `https://rickandmortyapi.com/api/character?page=`
+  );
+
   useEffect(() => {
-    fetch(`https://rickandmortyapi.com/api/character?page=${next}`)
-      .then((response) => response.json())
-      .then((response) => setCharacterList(response.results))
-      .catch((err) => console.log(err));
-  }, [next]);
-  const nextPage = () => {
-    setNext(next + 1);
-  };
-  const previousPage = () => {
-    if (next > 1) {
-      setNext(next - 1);
+    if (link !== null) {
+      fetch(`${link}`)
+        .then((response) => response.json())
+        .then((response) => {
+          setCharacterList([...characterList, ...response.results]);
+          setLink(response.info.next);
+        });
     }
-  };
+  }, [characterList, link]);
 
   return (
     <div className="App-header">
-      <div className="buttons">
-        <button onClick={previousPage}>Previous Page</button>
-        <button onClick={nextPage}>Next Page</button>
-      </div>
+      <div className="buttons"></div>
       <Characters characterList={characterList} />
     </div>
   );
